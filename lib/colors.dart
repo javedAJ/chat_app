f344252c800de29804d2b66f07f77c12b9ce809d
javedAJ <javedatrt@gmail.com>
2022-02-22 11:59:34 +0530

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color lightpink = const Color(0xFFf39f82);
+  static Color lightblack = const Color(0xFF4b4b4b);
+  static Color lightwhite = const Color(0xFFfffbf9);
+  static Color lightblue = const Color(0xFF7da6cf);
+  static Color lightpurple = const Color(0xFFdeaac5);
+}
